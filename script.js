0c2fd95b0c65feca30d92c9ef1ad64503ce2fbb4
rostand kinfack <kinfackrostand@gmail.com>
2024-09-17 10:39:41 +0200

    g

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-export let options = {
-  // Configuration des VUs et de la durée
-  vus: 10,
-  duration: '30s',
-  // Configuration de l'output pour InfluxDB
-  influxdb: {
-    url: 'http://influxdb:8086',
-  }
-};
 
 export default function () {
   let res = http.get('https://test.k6.io');
